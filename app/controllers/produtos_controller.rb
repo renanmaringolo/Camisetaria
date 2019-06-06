74291ab0b30d277ae8209ae56e61750f19e1f7f6
Renan Maríngolo <renan.maringolo@gmail.com>
2019-06-05 23:05:13 -0300

    finalExercise

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,4 +1,6 @@
 class ProdutosController < ApplicationController
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
     def index
         @produtos_por_nome = Produto.order(:nome).limit 5
         @produtos_por_preco = Produto.order(:preco).limit 2
@@ -6,28 +8,55 @@ class ProdutosController < ApplicationController
 
     def new
         @produto = Produto.new
-        @departamentos = Departamento.all
-    end
-
-    def create
-        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
-        @produto = Produto.new valores
-        if @produto.save
-            flash[:notice] = "Produto salvo com sucesso"
-            redirect_to root_url
-        else
-            render :new
-        end
-    end
-
-    def destroy
-        id = params[:id]
-        Produto.destroy id
-        redirect_to root_url
+        renderiza :new
     end
 
     def busca
         @nome_a_buscar = params[:nome]
         @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+    end
+
+    def edit
+        renderiza :edit
+    end
+
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto Atualizado com sucesso!"
+            redirect_to root_url
+        else
+            renderiza :edit
+        end
+    end
+
+    def destroy
+        @produto.destroy
+        redirect_to root_url
+    end
+
+    def create
+        @produto = Produto.new produto_params
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso"
+            redirect_to root_url
+        else
+            renderiza :new
+        end
+    end
+
+    private
+
+    def renderiza(view)
+        @departamentos = Departamento.all   
+        render view
+    end
+
+    def set_produto
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+
+    def produto_params
+        valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
     end
 end
